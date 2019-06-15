4b927f9d2e395fdf2d79d63af0fe778d895429a8
Ilya E <ermoker@yandex.ru>
2019-06-15 19:55:40 +0200

    MERGE: SQlite db file, ApiObject classes, Api/login route (#4)
    Squash 1 commit

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Api = require("./../helpers/apiObjects");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Api' });
+	let response = new Api.Response(200, "Все ок", new Api.Default("Pong"));
+	res.send(response);
+});
+
+router.get('/login', function(req, res, next) {
+	let response = new Api.Response(200, "Все ок", new Api.Login(req.body.login, req.body.password));
+	res.send(response);
 });
 
 module.exports = router;
