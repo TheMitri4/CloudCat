6e997802f89cbcd709ccbd399a2ed3a4b71d9bbc
Ilya E <ermoker@yandex.ru>
2019-06-15 20:24:59 +0200

    HOTFIX: api.post (#6)

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var Api = require("./../helpers/apiObjects");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
 	let response = new Api.Response(200, "Все ок", new Api.Default("Pong"));
 	res.send(response);
 });
 
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
 	let response = new Api.Response(200, "Все ок", new Api.Login(req.body.login, req.body.password));
 	res.send(response);
 });
