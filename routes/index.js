a934475c9216b1cee005767dea07f08ecd04ffec
TheMitri4 <DimaMK98@gmail.com>
2019-06-16 03:07:35 +0300

    loginPage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+	res.render('login', { title: 'Express' });
+});
+
+// авторизация пользователя
+
+router.post('/login', function(req, res, next) {
+	console.log(req.body);
+	let options = {
+		method: 'POST',
+		uri: 'http://localhost:3000/api/login',
+		form: {
+			login: req.body.login,
+			password: req.body.password
+		}
+	}
+	rp(options)
+		.then(function(result){
+			res.send(result);
+		})
+		.catch(function(err){
+			console.log(err);
+			res.send('Ошибка');
+		})
+});
+
+// Получаем главную страницу
+
+router.get('/main-page', function(req, res, next) {
+	res.render('main-page', { login: 'Express' });
 });
 
 module.exports = router;
